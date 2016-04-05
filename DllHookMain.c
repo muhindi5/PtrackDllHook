321e8a7d95e56b61117eb5c77c8f1694fd9e1964
Kelly Muhindi <kmuhindi@jkuat.ac.ke>
2016-04-05 17:12:21 +0300

    Extracted process id from active window + window caption

--- DllHookMain.c
+++ DllHookMain.c
@@ -3,12 +3,11 @@
 #include "WinHook.h"
 #include <stdio.h>
 #include <time.h>
-
 //max characters to include in title bar buffer
 #define MAX_BUFFER_SIZE 200
 
-BOOL writeToLog(LPTSTR);
-VOID generateRandomStr(char holder[]);
+BOOL logToFile(LPTSTR);
+VOID generateRandomStr(char*);
 
 /*Callback Function: CallWndProc 
   Get title bar caption from the Window currently activated
@@ -22,14 +21,16 @@ LRESULT CALLBACK GetMsgProc(int code, WPARAM wParam, LPARAM lParam) {
 	if (code == HC_ACTION) {
 		CWPSTRUCT* cwps = (CWPSTRUCT*)lParam;
 		if ((cwps->message == WM_ACTIVATE) && (LOWORD(cwps->wParam) == WA_INACTIVE)) {
-		OutputDebugString(L"Code==message == WM_ACTIVATE and wParam==WA_ACTIVE");
 				HWND hActivatedWindow = (*cwps).hwnd; // dereference pointer and get handle to window being activated
-				if (hActivatedWindow != NULL) {
-					OutputDebugString(L"Window handle not null");
-				}
 				if (GetWindowText(hActivatedWindow, szWincapText, MAX_BUFFER_SIZE) != 0) {
-					Beep(2000, 300);
-					OutputDebugString(szWincapText);
+					if (hActivatedWindow == NULL) { return 0; }
+
+					Beep(3000, 400);
+					DWORD pProcId = 0;
+					GetWindowThreadProcessId(hActivatedWindow,&pProcId);
+					wchar_t buffer[256];
+					wsprintf(buffer, L"%lu:%s",pProcId, szWincapText);
+					OutputDebugString(buffer);
 				}
 				return 0;
 		}
@@ -37,34 +38,35 @@ LRESULT CALLBACK GetMsgProc(int code, WPARAM wParam, LPARAM lParam) {
 	return CallNextHookEx(NULL, code, wParam, lParam);
 }
 /*Create random-named file and write string (caption text in parameter)*/
-BOOL writeToLog(LPTSTR szWinText) {
-	char* content = "";
-	char generatedFileName[10];
-	char dirPath[] = "C:\\Users\\Public\\tester\\";
-	char filePath[38];
-	generateRandomStr(generatedFileName);	
-	snprintf(filePath, sizeof(filePath), "%s%s.txt", dirPath, generatedFileName);
-	HANDLE fHandle = CreateFile((LPTSTR)filePath,GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	//format text and write to file
-	snprintf(content, MAX_BUFFER_SIZE, "window title: %s", (char*)szWinText);
-	WriteFile(fHandle, content, sizeof(content),NULL,NULL);
-	CloseHandle(fHandle);
+BOOL logToFile(LPTSTR szWinCaption) {
+	char dir [] = "C:\\Users\\Public\\tester\\";
+	char fileName[10] = "";
+	generateRandomStr(fileName); //get generated file name
+	////strncat_s(dir,strlen(dir),fileName,36);
+	//snprintf(dir,strlen(dir), "%s%s.txt", dir, fileName);
+	//HANDLE fHandle = CreateFile((LPTSTR)dir,GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	////format text and write to file
+	////snprintf(captionText, MAX_BUFFER_SIZE, "Window Title: %s",winCaption);
+	//WriteFile(fHandle, szWinCaption,MAX_BUFFER_SIZE,NULL,NULL);
+	//CloseHandle(fHandle);
 	return TRUE;
 }
 
 
 /*Generate a log file name for the process */
-VOID generateRandomStr(char holder[]) {
-	//OutputDebugString((LPCWSTR)TEXT("Inside string generator...\n"));
+VOID generateRandomStr(char holder []) {
+	OutputDebugString(L"Inside string generator");
 	size_t i = 0;
 	(time(NULL)); //seed random number generator using system clock seconds
 	while (i < 9) {
 		int x = (97 + rand() % 97);
 		if (x > 96 && x < 122) {
 			holder[i] = ((char)x);
-			//puts((char)x);
 			i++;
 		}
 	}
 	holder[i++] = '\0'; //terminate the string
 }
+
+
+
